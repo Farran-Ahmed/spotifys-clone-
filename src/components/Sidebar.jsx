bfe5db98c46b3783c1b350ed779f381e5a311764
Farran Ahmed <Farranahmad20@gmail.com>
2026-07-13 17:06:08 -0500

    Add Sidebar component with basic title

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+function Sidebar() {
+  //
+  return (
+    <div className="sidebar">
+      <h2>my spotify clone</h2>
+    </div>
+  );
+}
+export default Sidebar;
+
+//this funtion is called sidebar for the sidebar compants
+// use div className so we can style it later becayse classname allowes use CSS
+//then export the bar to the app.jsx file
